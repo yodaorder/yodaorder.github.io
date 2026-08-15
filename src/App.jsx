7850a0379f889e0f3009d8ef0334f6f24914117c
GrandMasterYoda <gamerbrantleymcintyre6@gmail.com>
2026-08-15 17:48:49 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ function App() {
 
           <Route
             path="/partnerships"
-            element={<Partnership />}
+            element={<Partnerships />}
           />
 
           <Route
