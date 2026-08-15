bac40770e95897dfa052d960422e29127a846bcb
GrandMasterYoda <gamerbrantleymcintyre6@gmail.com>
2026-08-15 16:43:01 -0400

    Refactor App component structure and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,52 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
-
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
-import Teams from "./pages/Teams";
-import Members from "./pages/Members";
-import Partnerships from "./pages/Partnerships";
-import Contact from "./pages/Contact";
-import Admin from "./pages/Admin";
 import Login from "./pages/Login";
+import Admin from "./pages/Admin";
+
+import Footer from "./components/Footer";
+
 import ProtectedRoute from "./components/ProtectedRoute";
 
+
 function App() {
-    return (
-            <Navbar />
+  return (
+    <div>
 
-            <main>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/teams" element={<Teams />} />
-                    <Route path="/members" element={<Members />} />
-                    <Route path="/partnerships" element={<Partnerships />} />
-                    <Route path="/contact" element={<Contact />} />
-                    <Route
-                        path="/admin"
-                        element={
-                            <ProtectedRoute>
-                                <Admin />
-                            </ProtectedRoute>
-                        } 
-                    />
-                    <Route path="/login" element={<Login />} />
-                </Routes>
-            </main>
+      <main>
 
-            <Footer />
-    );
+        <Routes>
+
+          <Route
+            path="/"
+            element={<Home />}
+          />
+
+          <Route
+            path="/login"
+            element={<Login />}
+          />
+
+
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute>
+                <Admin />
+              </ProtectedRoute>
+            }
+          />
+
+        </Routes>
+
+      </main>
+
+
+      <Footer />
+
+    </div>
+  );
 }
+
 
 export default App;
