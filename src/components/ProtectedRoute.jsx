140b851f25c487681c04e28a9930e8fc466fea5c
GrandMasterYoda <gamerbrantleymcintyre6@gmail.com>
2026-08-15 17:56:11 -0400

    Update ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,53 +1,14 @@
 import { Navigate } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { checkAuth } from "../services/auth";
-
-
-const leadershipEmails = [
-  "contact.me.for.question@gmail.com",
-];
-
 
 function ProtectedRoute({ children }) {
-  const [user, setUser] = useState(undefined);
 
+  const loggedIn = localStorage.getItem("staffLoggedIn");
 
-  useEffect(() => {
-    const unsubscribe = checkAuth((currentUser) => {
-      setUser(currentUser);
-    });
-
-    return () => unsubscribe();
-  }, []);
-
-
-  // Wait for Firebase to check login
-  if (user === undefined) {
-    return <h2>Loading...</h2>;
-  }
-
-
-  // Not logged in
-  if (!user) {
+  if (loggedIn !== "true") {
     return <Navigate to="/login" replace />;
   }
 
-
-  // Logged in but not leadership
-  if (!leadershipEmails.includes(user.email)) {
-    return (
-      <div>
-        <h1>Access Denied</h1>
-        <p>
-          You do not have permission to access the Riftline admin panel.
-        </p>
-      </div>
-    );
-  }
-
-
   return children;
 }
-
 
 export default ProtectedRoute;
