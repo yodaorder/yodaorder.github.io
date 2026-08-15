e265f7ee0bcdafd8e78ad66164e5e9cb2f5d357c
GrandMasterYoda <gamerbrantleymcintyre6@gmail.com>
2026-08-15 05:21:16 -0400

    Refactor firebase.js for cleaner structure

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,8 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { db } from "../service/firebase";
-
 
 
 const firebaseConfig = {
@@ -15,16 +13,11 @@ const firebaseConfig = {
 };
 
 
-
 const app = initializeApp(firebaseConfig);
-
 
 
 export const auth = getAuth(app);
 
-
-
 export const db = getFirestore(app);
-
 
 export default app;
